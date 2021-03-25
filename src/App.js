79168d6928d178708a4b804ebbab23d7aef9387f
davidchungcode <david.chung93@gmail.com>
2021-03-25 16:08:11 -0700

    Updated sort functionality

--- src/App.js
+++ src/App.js
@@ -13,6 +13,15 @@ function App() {
   const [currencies, setCurrencies] = useState([])
   const [search, setSearch] = useState('')
   const [nightMode, setNightMode] = useState(false)
+  
+  const [sortRank, setSortRank] = useState(false)
+  const [sortName, setSortName] = useState(false)
+  const [sortSymbol, setSortSymbol] = useState(false)
+  const [sortPrice, setSortPrice] = useState(false)
+  const [sortPercentChange, setSortPercentChange] = useState(false)
+  const [sortMarketCap, setSortMarketCap] = useState(false)
+  const [sortVolume, setSortVolume] = useState(false)
+  const [sortSupply, setSortSupply] = useState(false)
 
   useEffect(() => {
     axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
@@ -31,50 +40,98 @@ function App() {
 
   function sortByRank() {
     filteredCurrencies = [...currencies]
-    filteredCurrencies.sort((a,b) => (a.market_cap_rank > b.market_cap_rank) ? 1 : -1)
+    if (sortRank) {
+      filteredCurrencies.sort((a,b) => (a.market_cap_rank > b.market_cap_rank) ? 1 : -1)
+    }
+    else {
+      filteredCurrencies.sort((a,b) => (a.market_cap_rank < b.market_cap_rank) ? 1 : -1)
+    }
     setCurrencies(filteredCurrencies)
+    setSortRank(!sortRank)
   }
 
   function sortByName() {
     filteredCurrencies = [...currencies]
-    filteredCurrencies.sort((a,b) => (a.name > b.name) ? 1 : -1)
+    if (sortName) {
+      filteredCurrencies.sort((a,b) => (a.name.toLowerCase() < b.name.toLowerCase()) ? 1 : -1)
+    }
+    else {
+      filteredCurrencies.sort((a,b) => (a.name.toLowerCase() > b.name.toLowerCase()) ? 1 : -1)
+    }
     setCurrencies(filteredCurrencies)
+    setSortName(!sortName)
   }
 
   function sortBySymbol() {
     filteredCurrencies = [...currencies]
-    filteredCurrencies.sort((a,b) => (a.symbol > b.symbol) ? 1 : -1)
+    if (sortSymbol) {
+      filteredCurrencies.sort((a,b) => (a.symbol.toLowerCase() < b.symbol.toLowerCase()) ? 1 : -1)
+    }
+    else {
+      filteredCurrencies.sort((a,b) => (a.symbol.toLowerCase() > b.symbol.toLowerCase()) ? 1 : -1)
+    }
     setCurrencies(filteredCurrencies)
+    setSortSymbol(!sortSymbol)
   }
 
   function sortByPrice() {
     filteredCurrencies = [...currencies]
-    filteredCurrencies.sort((a,b) => (a.current_price < b.current_price) ? 1 : -1)
+    if (sortPrice) {
+      filteredCurrencies.sort((a,b) => (a.current_price > b.current_price) ? 1 : -1)
+    }
+    else {
+      filteredCurrencies.sort((a,b) => (a.current_price < b.current_price) ? 1 : -1)
+    }
     setCurrencies(filteredCurrencies)
+    setSortPrice(!sortPrice)
   }
 
   function sortByPercentChange() {
     filteredCurrencies = [...currencies]
-    filteredCurrencies.sort((a,b) => (a.price_change_percentage_24h < b.price_change_percentage_24h) ? 1 : -1)
+    if (sortPercentChange) {
+      filteredCurrencies.sort((a,b) => (a.price_change_percentage_24h > b.price_change_percentage_24h) ? 1 : -1)
+    }
+    else {
+      filteredCurrencies.sort((a,b) => (a.price_change_percentage_24h < b.price_change_percentage_24h) ? 1 : -1)
+    }
     setCurrencies(filteredCurrencies)
+    setSortPercentChange(!sortPercentChange)
   }
 
   function sortByMarketCap() {
     filteredCurrencies = [...currencies]
-    filteredCurrencies.sort((a,b) => (a.market_cap < b.market_cap) ? 1 : -1)
+    if (sortMarketCap) {
+      filteredCurrencies.sort((a,b) => (a.market_cap > b.market_cap) ? 1 : -1)
+    }
+    else {
+      filteredCurrencies.sort((a,b) => (a.market_cap < b.market_cap) ? 1 : -1)
+    }
     setCurrencies(filteredCurrencies)
+    setSortMarketCap(!sortMarketCap)
   }
 
   function sortByVolume() {
     filteredCurrencies = [...currencies]
-    filteredCurrencies.sort((a,b) => (a.total_volume < b.total_volume) ? 1 : -1)
+    if (sortVolume) {
+      filteredCurrencies.sort((a,b) => (a.total_volume > b.total_volume) ? 1 : -1)
+    }
+    else {
+      filteredCurrencies.sort((a,b) => (a.total_volume < b.total_volume) ? 1 : -1)
+    }
     setCurrencies(filteredCurrencies)
+    setSortVolume(!sortVolume)
   }
 
   function sortBySupply() {
     filteredCurrencies = [...currencies]
-    filteredCurrencies.sort((a,b) => (a.circulating_supply < b.circulating_supply) ? 1 : -1)
+    if (sortSupply) {
+      filteredCurrencies.sort((a,b) => (a.circulating_supply > b.circulating_supply) ? 1 : -1)
+    }
+    else {
+      filteredCurrencies.sort((a,b) => (a.circulating_supply < b.circulating_supply) ? 1 : -1)
+    }
     setCurrencies(filteredCurrencies)
+    setSortSupply(!sortSupply)
   }
 
   return (
