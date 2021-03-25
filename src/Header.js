71025741257d1d7429972ee1e1e94ccb194853a6
davidchungcode <david.chung93@gmail.com>
2021-03-25 16:22:54 -0700

    Updated header description

--- src/Header.js
+++ src/Header.js
@@ -4,6 +4,11 @@ export default function Header() {
             <h1>Cryptocurrency Price Tracker</h1>
             <p>This project uses the </p>
             <a href='https://www.coingecko.com/en/api'>CoinGecko API</a>
+            <br></br>
+            <br></br>
+            <div className='description'>Feel free to search for a specific Cryptocurrency, sort through them, or click one for more details about it.</div>
+            <br></br>
+            <div className='description'>There is also Dark Mode toggle in the top right corner.</div>
         </div>
     )
 }
